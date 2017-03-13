dec53099d912be5e93196920faa94ba30616901a
David Abrahams <davidabrahams@sbcglobal.net>
2017-03-13 13:46:08 -0700

    starting word ladder

--- WordLadder/src/wordladder.cpp
+++ WordLadder/src/wordladder.cpp
@@ -1,12 +1,38 @@
-// This is the CPP file you will edit and turn in. (TODO: Remove this comment!)
-
 #include <iostream>
 #include "console.h"
+#include "simpio.h"
+#include <fstream>
+#include <set>
+#include <queue>
+#include <stack>
 using namespace std;
 
-int main() {
-    // TODO: Finish the program!
 
-    cout << "Have a nice day." << endl;
+void readDictionary(set<string> &dic);
+void wordLadder(string from, string to);
+
+
+int main() {
+    set<string> dictionary;
+    readDictionary(dictionary);
     return 0;
+}
+
+void wordLadder(string from, string to) {
+    queue<stack<string>> q;
+    stack<string> stack;
+    stack.push(to);
+    q.push(stack);
+    while (!q.empty()) {
+    }
+}
+
+void readDictionary(set<string> &dic) {
+    string fn = getLine("Enter the filename for your dictionary:");
+    ifstream file;
+    file.open(fn);
+    string word;
+    while (file >> word)
+        dic.insert(word);
+    cout << "Dictionary saved." << endl;
 }
