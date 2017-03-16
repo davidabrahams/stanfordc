9a13696933e3a9f0a9fcbba41d287cc4a32cb0a1
David Abrahams <davidabrahams@sbcglobal.net>
2017-03-16 15:49:45 -0700

    computer functionality done!

--- Boggle/src/bogglesearch.cpp
+++ Boggle/src/bogglesearch.cpp
@@ -10,6 +10,9 @@ using namespace std;
 
 void placeCubes(const Vector<std::string>& letterCubes, Grid<char>& grid);
 bool containsWords(const Grid<char>& board, Grid<bool>& visited, const string& word, int r, int c, int i);
+void findWords(const Grid<char>& board, Grid<bool>& visited, Set<string>& foundWords,
+               const Set<string>& humanWords, const Lexicon& dictionary, string& word, int r, int c);
+
 
 Grid<char> generateRandomBoard(int size, const Vector<std::string>& letterCubes) {
     // TODO: write this function
@@ -20,6 +23,7 @@ Grid<char> generateRandomBoard(int size, const Vector<std::string>& letterCubes)
 }
 
 bool humanWordSearch(const Grid<char>& board, const Lexicon& dictionary, const string& w) {
+    BoggleGUI::setAnimationDelay(100);
     string word = toUpperCase(w);
     if (!dictionary.contains(word)) return false;
     if (word.length() < BoggleGUI::MIN_WORD_LENGTH) return false;
@@ -42,9 +46,25 @@ bool humanWordSearch(const Grid<char>& board, const Lexicon& dictionary, const s
 }
 
 Set<string> computerWordSearch(const Grid<char>& board, const Lexicon& dictionary, const Set<string>& humanWords) {
-    // TODO: write this function
-    Set<string> todo;   // this is only here so it will compile
-    return todo;        // this is only here so it will compile
+
+    BoggleGUI::setAnimationDelay(10);
+    Set<string> foundWords;   // this is only here so it will compile
+    Grid<bool> visited = Grid<bool>(board.numRows(), board.numCols());
+    cout << visited.toString2D() << endl;
+    string word = "";
+    for (int r = 0; r < board.numRows(); r++) {
+        for (int c = 0; c < board.numCols(); c++) {
+            findWords(board, visited, foundWords, humanWords, dictionary, word, r, c);
+        }
+    }
+    for (string w : foundWords) {
+        if (w.length() > 7) BoggleGUI::scorePointsComputer(11);
+        else if (w.length() == 7) BoggleGUI::scorePointsComputer(5);
+        else if (w.length() == 6) BoggleGUI::scorePointsComputer(3);
+        else if (w.length() == 5) BoggleGUI::scorePointsComputer(2);
+        else BoggleGUI::scorePointsComputer(1);
+    }
+    return foundWords;        // this is only here so it will compile
 }
 
 void placeCubes(const Vector<std::string>& letterCubes, Grid<char>& grid) {
@@ -62,7 +82,6 @@ bool containsWords(const Grid<char>& board, Grid<bool>& visited, const string& w
     if (c==-1 || c==board.numCols()) return false;
     if (visited[r][c]) return false;
     BoggleGUI::setHighlighted(r, c, true);
-    BoggleGUI::setAnimationDelay(50);
     if (board[r][c] != word[i]){
         BoggleGUI::setHighlighted(r, c, false);
         return false;
@@ -80,4 +99,33 @@ bool containsWords(const Grid<char>& board, Grid<bool>& visited, const string& w
     visited[r][c] = false;
     BoggleGUI::setHighlighted(r, c, false);
     return false;
+}
+
+void findWords(const Grid<char>& board, Grid<bool>& visited, Set<string>& foundWords,
+               const Set<string>& humanWords, const Lexicon& dictionary, string& word, int r, int c) {
+    if (r==-1 || r==board.numRows()) return;
+    if (c==-1 || c==board.numCols()) return;
+    if (visited[r][c]) return;
+    word.push_back(board[r][c]);
+    if (!dictionary.containsPrefix(word)) {
+        word.pop_back();
+        return;
+    }
+    cout << endl;
+    if (word.length() >= BoggleGUI::MIN_WORD_LENGTH && dictionary.contains(word) && !humanWords.contains(word)) {
+        foundWords.add(word);
+    }
+    visited[r][c] = true;
+    BoggleGUI::setHighlighted(r, c, true);
+    findWords(board, visited, foundWords, humanWords, dictionary, word, r-1, c-1);
+    findWords(board, visited, foundWords, humanWords, dictionary, word, r-1, c);
+    findWords(board, visited, foundWords, humanWords, dictionary, word, r-1, c+1);
+    findWords(board, visited, foundWords, humanWords, dictionary, word, r, c-1);
+    findWords(board, visited, foundWords, humanWords, dictionary, word, r, c+1);
+    findWords(board, visited, foundWords, humanWords, dictionary, word, r+1, c-1);
+    findWords(board, visited, foundWords, humanWords, dictionary, word, r+1, c);
+    findWords(board, visited, foundWords, humanWords, dictionary, word, r+1, c+1);
+    BoggleGUI::setHighlighted(r, c, false);
+    visited[r][c] = false;
+    word.pop_back();
 }
